c10abb094979e1472e94213096b55f87fd54d69d
makis-san <svs4@discente.ifpe.edu.br>
2020-12-05 01:56:39 -0300

    tracking correios

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
-
 import Main from './components/navigation';
+
 
 export default function App() {
   return (
