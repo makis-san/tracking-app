74557b9439197f02055493e20c00cc133a861e4f
makis-san <svs4@discente.ifpe.edu.br>
2020-12-05 04:03:19 -0300

    Context Api

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { View } from 'react-native';
 import Main from './components/navigation';
-
+import { userData, ParcelData } from './@types/index';
+import { AsyncStorage } from "react-native";
+import AppContext from './components/AppContext';
 
 export default function App() {
+
+  const [data, setData] = useState<userData>();
+  const userSettings:any = {
+    'data': data,
+    setData,
+  };
   return (
+    <AppContext.Provider value={userSettings}>
       <Main/>
+    </AppContext.Provider>
   );
 }
 
